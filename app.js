dd26e7a53501da5710e710503fc97dd14b058e48
abnetayele <abnetayele694@gmail.com>
2025-01-08 15:39:30 -0800

    how to use simple listen method

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const http = require('http')
-const express = require('express')
-const app = express(); 
-app.use((req,res,next)=>{
-    console.log(" i am in the mddelware");
-    next()
-})
-app.use((req,res,next)=>{
-    console.log("second middelware1");
+const http = require('http');
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+    console.log("I am in the first middleware");
+    next();
+});
+
+
+app.use((req, res, next) => {
+    console.log("I am in the second middleware");
+    next();
+});
+app.use((req, res) => {
+    console.log("I am in the third middleware, responding now.");
+     res.send("Hello from the server!");
+});
+
+
+// const server = http.createServer(app);
+// server.listen(3000, () => {
+//     console.log("Server is running on port 3000");
+// });
+app.listen((3000),()=>{
+    console.log("server is runnig  3000");
     
-})
-const server= http.createServer(app)
-server.listen((3000),()=>{
-    console.log("server is renning in port 3000");
 })
